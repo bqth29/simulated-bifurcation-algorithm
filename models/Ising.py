d564bede8e2251b90a8c88172608bd2fe2d2ee9a
Thomas Bouquet <thomas.bouquet@student-cs.fr>
2021-11-10 17:59:49 +0100

    time step

--- models/Ising.py
+++ models/Ising.py
@@ -87,7 +87,7 @@ class Ising():
                         X += symplectic_time_step * detuning_frequency * Y
                         Y -= symplectic_time_step * (kerr_constant * X**3 - factor * X)  
 
-                    Y += xi0 * (self.J @ X - 2 * pow(factor / kerr_constant, .5) * self.h) * time_step
+                    Y += time_step * xi0 * (self.J @ X - 2 * pow(factor / kerr_constant, .5) * self.h)
 
                     # Check the stop criterion
 
