03a2e345fe515238bbcad816bc329dcef688a397
Thomas Bouquet <thomas.bouquet@student-cs.fr>
2021-12-12 19:17:00 +0100

    .

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ import numpy as np
 markowitz = Markowitz.from_csv(assets_list = assets[:], number_of_bits = 1, date = dates[-1], risk_coefficient = 1)
 markowitz.optimize(
     window_size = 35,
-    sample_frequency = 60,
+    sampling_period = 60,
     time_step = 0.01,
     symplectic_parameter = 2,
     pressure = lambda t : 0.0088 * t
