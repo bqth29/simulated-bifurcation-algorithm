6b8068c48efb572799d3ffcf9af99f85b17bc762
Thomas Bouquet <thomas.bouquet@student-cs.fr>
2023-06-25 21:31:51 +0200

    readme for packaging

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 import setuptools
 
 
+with open("README.md", "r", encoding='utf-8') as readme:
+    long_description = readme.read()
+
 setuptools.setup(
     name="simulated-bifurcation",
-    version="1.1.0",
+    version="1.1.1",
     description="Efficient implementation of the quantum-inspired Simulated Bifurcation (SB) algorithm to solve Ising-like problems.",
     url="https://github.com/bqth29/simulated-bifurcation-algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Apache License :: 2.0",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.8"
 )
