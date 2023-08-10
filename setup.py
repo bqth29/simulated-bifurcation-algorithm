6d7b347432b8f9e0b7194dad057685b23406882d
nightingale38 <romain.ageron@student-cs.fr>
2023-08-10 10:24:00 -0700

    update classifiers + remove pytest installation from config.yml

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
-
-with open("README.md", "r", encoding='utf-8') as readme:
+with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
 setuptools.setup(
     name="simulated-bifurcation",
     version="1.2.0",
-    description="Efficient implementation of the quantum-inspired Simulated Bifurcation (SB) algorithm to solve Ising-like problems.",
+    description="Efficient implementation of the quantum-inspired Simulated "
+    "Bifurcation (SB) algorithm to solve Ising-like problems.",
     url="https://github.com/bqth29/simulated-bifurcation-algorithm",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,15 +15,13 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
     ],
-    install_requires=[
-        "tqdm",
-        "numpy",
-        "torch>=2.0.1",
-        "coverage",
-        "pytest"
-    ],
+    install_requires=["tqdm", "numpy", "torch>=2.0.1", "coverage", "pytest"],
     python_requires=">=3.8",
-    package_dir={"":"src"}
+    package_dir={"": "src"},
 )
