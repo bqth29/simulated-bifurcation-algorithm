424bc4a7be4b2794b7f4b1ae645e8d96ab416ee2
Thomas Bouquet <thomas.bouquet@student-cs.fr>
2023-09-18 03:59:25 +0200

    more realistic test

--- tests/models/test_sequential_markowitz.py
+++ tests/models/test_sequential_markowitz.py
@@ -3,7 +3,7 @@ import torch
 from src.simulated_bifurcation.models import SequentialMarkowitz
 
 #  2 assets on 4 timestamps case test
-initial_stocks = torch.tensor([1.0, 0.0])
+initial_stocks = torch.tensor([0.0, 1.0])
 covariances = torch.tensor(
     [
         [[1.0, 0.2], [0.2, 1]],
@@ -22,10 +22,10 @@ expected_returns = torch.tensor(
 )
 rebalancing_costs = torch.tensor(
     [
-        [[0.1, 0.4], [0.4, 0.2]],
-        [[0.11, 0.38], [0.38, 0.2]],
-        [[0.05, 0.32], [0.32, 0.4]],
-        [[0.01, 0.4], [0.4, 0.5]],
+        [[0.1, 0.0], [0.0, 0.2]],
+        [[0.11, 0.0], [0.0, 0.2]],
+        [[0.05, 0.0], [0.0, 0.4]],
+        [[0.01, 0.0], [0.0, 0.5]],
     ]
 )
 
@@ -38,7 +38,7 @@ def test_sequential_markowitz():
         rebalancing_costs,
         initial_stocks,
         risk_coefficient=1,
-        number_of_bits=3,
+        number_of_bits=1,
     )
 
     assert torch.equal(covariances, model.covariances)
@@ -52,14 +52,14 @@ def test_sequential_markowitz():
         torch.isclose(
             torch.tensor(
                 [
-                    [-0.71, -0.88, 0.11, 0.38, 0.0, 0.0, 0.0, 0.0],
-                    [-0.88, -0.90, 0.38, 0.20, 0.0, 0.0, 0.0, 0.0],
-                    [0.11, 0.38, -0.66, -0.85, 0.05, 0.32, 0.0, 0.0],
-                    [0.38, 0.20, -0.85, -1.10, 0.32, 0.40, 0.0, 0.0],
-                    [0.0, 0.0, 0.05, 0.32, -0.56, -0.895, 0.01, 0.40],
-                    [0.0, 0.0, 0.32, 0.40, -0.895, -1.40, 0.40, 0.50],
-                    [0.0, 0.0, 0.0, 0.0, 0.01, 0.40, -0.51, -0.34],
-                    [0.0, 0.0, 0.0, 0.0, 0.40, 0.50, -0.34, -1.0],
+                    [-0.71, -0.10, 0.11, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [-0.10, -0.90, 0.0, 0.20, 0.0, 0.0, 0.0, 0.0],
+                    [0.11, 0.0, -0.66, -0.15, 0.05, 0.0, 0.0, 0.0],
+                    [0.0, 0.20, -0.15, -1.10, 0.0, 0.40, 0.0, 0.0],
+                    [0.0, 0.0, 0.05, 0.0, -0.56, -0.175, 0.01, 0.0],
+                    [0.0, 0.0, 0.0, 0.40, -0.175, -1.40, 0.0, 0.50],
+                    [0.0, 0.0, 0.0, 0.0, 0.01, 0.0, -0.51, 0.06],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.50, 0.06, -1.0],
                 ]
             ),
             model.matrix,
@@ -68,16 +68,16 @@ def test_sequential_markowitz():
     assert torch.all(
         torch.isclose(
             torch.tensor(
-                [0.7000, 1.4000, 0.5500, 0.5500, 0.7000, 0.5000, 1.4000, 0.2000]
+                [0.5000, 1.0000, 0.5500, 0.5500, 0.7000, 0.5000, 1.4000, 0.2000]
             ),
             model.vector,
         )
     )
-    assert torch.equal(torch.tensor(-0.1), model.constant)
+    assert torch.equal(torch.tensor(-0.2), model.constant)
 
-    model.maximize(agents=64, use_window=False, verbose=False)
+    model.maximize(agents=128, use_window=False, verbose=False)
     assert (4, 2) == model.portfolio.shape
     assert torch.equal(
-        torch.tensor([[0.0, 2.0], [2.0, 0.0], [0.0, 2.0], [2.0, 0.0]]), model.portfolio
+        torch.tensor([[0.0, 1.0], [0.0, 0.0], [1.0, 0.0], [1.0, 0.0]]), model.portfolio
     )
-    assert 2.52 == round(model.gains, 4)
+    assert 0.95 == round(model.gains, 4)
