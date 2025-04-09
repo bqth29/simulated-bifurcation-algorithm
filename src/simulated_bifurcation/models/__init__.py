f49e36eaca95641aac385eb24215dd80783acd6b
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-04-09 16:46:14 +0200

    remove Knapsack

--- src/simulated_bifurcation/models/__init__.py
+++ src/simulated_bifurcation/models/__init__.py
@@ -11,7 +11,6 @@ yet.
 
 from .abc_model import ABCModel
 from .ising import Ising
-from .knapsack import Knapsack
 from .markowitz import Markowitz, SequentialMarkowitz
 from .number_partitioning import NumberPartitioning
 from .qubo import QUBO
